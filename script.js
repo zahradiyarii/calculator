baa58c2004292eaec545f2842f8f1131fcce2517
zahradiyarii <128123549+zahradiyarii@users.noreply.github.com>
2023-03-17 02:14:39 -0700

    Add files via upload
    script

---
+++ script.js
@@ -0,0 +1,12 @@
+let boxResult=document.getElementById("box-result");
+let sin=document.querySelector("#sin");
+let pow=document.querySelector('#pow')
+
+sin.addEventListener('click',function (){
+    boxResult.value=eval(boxResult.value);
+    boxResult.value=Math.sin(boxResult.value*Math.PI/180);
+})
+
+pow.addEventListener('click', function (){
+    boxResult.value=eval(boxResult.value)+"*"
+})
